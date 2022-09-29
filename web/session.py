34123a1dad8464ed240ea32cac7f11ebc6cf3a80
allenhichard <allenhichard21@gmail.com>
2022-09-29 03:36:02 -0300

    feat: remove requests verification

--- web/session.py
+++ web/session.py
@@ -3,12 +3,21 @@ from court import dict_courts
 from web import extraction as soup_web
 import bs4 as bs
 
+import urllib3
+from urllib3.util.ssl_ import create_urllib3_context
+
+ctx = create_urllib3_context()
+ctx.load_default_certs()
+ctx.options |= 0x4  # ssl.OP_LEGACY_SERVER_CONNECT
+
+
 
 class Session:
 
     def __init__(self, cnj):
         self.court = None
-        self.session = requests.Session()
+        #self.session = requests.Session()
+        self.session = urllib3.PoolManager(ssl_context=ctx)
         self.cnj = cnj.replace(".", "").replace("-", "")
         self.type_court = self.cnj[14:16]
         self.degrees_court = dict_courts[self.type_court]
@@ -18,17 +27,18 @@ class Session:
     def consult_process(self):
         for index, degree_court in enumerate(self.degrees_court):
             self.court = degree_court.ConfigurationRequisition(self.cnj)
-            response = self.session.get(self.court.url_request, verify=False)
-            if response.status_code == 200:
-                html = response.text
+            response = self.session.request("GET", self.court.url_request)
+            if response.status == 200:
+                html = response.data.decode("utf-8")
+                #print(html)
                 soap = bs.BeautifulSoup(html, "html.parser")
                 if "Não existem informações disponíveis para os parâmetros informados." in html:
                     continue
                 elif "processoSelecionado" in html:
                     selected_process = soap.find(id="processoSelecionado")["value"]
-                    response = self.session.get(self.court.sub_query(selected_process), verify=False)
-                    if response.status_code == 200:
-                        html = response.text
+                    response = self.session.request("GET", self.court.sub_query(selected_process))
+                    if response.status == 200:
+                        html = response.data.decode("utf-8")
                 extraction = soup_web.Extraction(html)
                 extraction.load()
                 key_result = self.court.state + " " + self.court.degree
