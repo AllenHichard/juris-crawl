d2f3f7da7e4f4a2ae02759cb368570aa1569cd7e
allenhichard <allenhichard21@gmail.com>
2022-09-28 23:17:51 -0300

    feat: configuring heroku

--- server/instance.py
+++ server/instance.py
@@ -8,6 +8,6 @@ class Server:
 
     def run(self):
         self.app.run(
-            host='0.0.0.0', port=8080,
-            debug=True
+            host='0.0.0.0',
+            port=8080
         )
