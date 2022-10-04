8d833506965ac34dbb75fb741488ed8ea98972aa
allenhichard <allenhichard21@gmail.com>
2022-10-04 00:02:27 -0300

    test: config deploy

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def default():
-    session = requests.Session()
-    session.trust_env = False
-    r = session.get("https://www2.tjal.jus.br/cpopg/open.do", proxies= { 'no_proxy': "*", } )
-    return {"Status": "access the api route"}
-
+    #session = requests.Session()
+    #session.trust_env = False
+    #r = session.get("https://www2.tjal.jus.br/cpopg/open.do")
+    #return {"Status": "access the api route"}
+    return dict(os.environ)
 
 
 @app.route("/api", methods=["GET"])
